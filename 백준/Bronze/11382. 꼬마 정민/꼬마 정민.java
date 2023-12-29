986014ca008c70fbbcd93fe6c2a9d187b99b7b46
leetanmi <leechanmi@dankook.ac.kr>
2023-12-29 18:47:17 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long nA = sc.nextLong();
+        long nB = sc.nextLong();
+        long nC = sc.nextLong();
+
+        System.out.println(nA+nB+nC);
+    }
+}
